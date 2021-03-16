2c06e367b2baeb3b2216474a00beb8176edb7b32
Hari Shankar <71510856+HariShankar08@users.noreply.github.com>
2021-03-16 12:07:05 +0530

    Update 16Mar.sql

--- SQL/16Mar.sql
+++ SQL/16Mar.sql
@@ -26,7 +26,7 @@ SELECT * FROM student1 WHERE name LIKE '%n'
 SELECT * FROM student1 WHERE name LIKE '%h%';
 
 INSERT INTO student1 (No, Name, Stipend, Stream, AvgMark, Class) VALUES
-(10, 'A', 350.00, 'Nonmedical', 88.5, '12A');
+(11, 'A', 350.00, 'Nonmedical', 88.5, '12A');
 
 -- Checking NULL
 SELECT * FROM student1 WHERE Grade IS NULL;
@@ -41,3 +41,27 @@ SELECT * FROM student1 WHERE Stipend BETWEEN 300 AND 400;
 -- ORDER BY Clause
 SELECT * FROM student1 ORDER BY name;
 SELECT * FROM student1 ORDER BY name DESC;
+
+SELECT stream, name FROM student1 ORDER BY stream, name DESC;
+
+-- SUM(), AVG(), MIN(), MAX()
+SELECT MAX(stipend) FROM student1;
+SELECT MIN(stipend) FROM student1;
+SELECT SUM(stipend) FROM student1;
+SELECT AVG(stipend) FROM student1;
+
+-- DATES
+CREATE TABLE foo (
+  rollno INT UNIQUE,
+  dob DATE,
+  class CHAR(3)
+);
+
+INSERT INTO foo VALUES
+(1, '2000-11-12', '11A'),
+(2, '2001-12-03', '11B'),
+(3, '1999-02-02', '12A'),
+(4, '2000-12-23', '12B');
+
+SELECT * FROM foo WHERE MONTH(dob) = 12;
+SELECT * FROM foo WHERE MONTHNAME(dob) = 'DECEMBER';
